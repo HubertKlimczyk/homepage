dd924fab33ee6ba331aeca02373ccbd61b915a59
Hubert Klimczyk <hklimczyk1@gmail.com>
2023-10-18 19:14:20 +0200

    JS updata

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
-let button = document.querySelector(".button");
-let white = document.querySelector(".container");
-let dark = document.querySelector(".container--dark");
-let darkIcon = document.querySelector(".button__icon--dark");
-let whiteIcon = document.querySelector(".button__icon");
-let whiteAsideLinks = document.querySelector(".aside__link");
-let DarkAsideLinks = document.querySelector(".aside__link--dark");
-let whiteTableHeader = document.querySelector(".table__cell--header");
-let darkTableHeader = document.querySelector(".table__cell--dark");
-let someString = document.querySelector(".button__emptyString");
+{
+    const toggleBackground = () => {
+        const white = document.querySelector(".js-container");
+        const whiteIcon = document.querySelector(".js-button__icon");
+        const whiteAsideLinks = document.querySelector(".js-aside__link");
+        const whiteTableHeader = document.querySelector(".js-table__cell--header");
 
-button.addEventListener("click", () => {
-    white.classList.toggle("container--dark");
-    whiteIcon.classList.toggle("button__icon--dark");
-    whiteAsideLinks.classList.toggle("aside__link--dark");
-    whiteTableHeader.classList.toggle("table__cell--dark");
+        white.classList.toggle("container--dark");
+        whiteIcon.classList.toggle("button__icon--dark");
+        whiteAsideLinks.classList.toggle("aside__link--dark");
+        whiteTableHeader.classList.toggle("table__cell--dark");
 
-    if (white.classList.contains("container--dark")) {
-        someString.innerText = "Napis testowy";
-    } else {
-        someString.innerText = "";
-    }
-});
+        testWord(white);
+    };
+
+    const testWord = white => {
+        const someString = document.querySelector(".js-button__emptyString");
+        if (white.classList.contains("container--dark")) {
+            someString.innerText = "Napis testowy";
+        } else {
+            someString.innerText = "";
+        }
+    };
+
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+    };
+
+    init();
+}
